cb223e6e247a45294a5fcc863a1c3be2de70b958
Ahmed Fathy <aliraqi.dev@gmail.com>
2018-09-06 17:35:36 +0200

    add laraboot:install command

---
+++ src/Console/Commands/LarabootInstallCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Laraboot\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class LarabootInstallCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'laraboot:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Publish the laraboot configuration and view files to your project.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // Generate authentication scaffolding.
+        $this->callSilent('make:auth', [
+            '--no-interaction' => true
+        ]);
+
+        // Publish the laraboot files.
+        $this->callSilent('vendor:publish', [
+            '--tag' => 'laraboot-scaffolding',
+            '--force' => '--force'
+        ]);
+
+        $this->info('laraboot was installed successfully.');
+    }
+}
